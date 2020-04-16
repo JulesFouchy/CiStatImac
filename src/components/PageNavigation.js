e540278b0d6994514dc3bd237dab1448feed9c39
Jules Fouchy <jules.fouchy@ntymail.com>
2020-04-16 17:38:41 +0200

    Mark the button as disabled when needed

--- src/components/PageNavigation.js
+++ src/components/PageNavigation.js
@@ -1,11 +1,15 @@
 import { h } from 'hyperapp'
 
 // basic componant with props
-export default (props) =>
-    h('div', {class: 'pageNavigation'}, [
-        h('span', {}, 'Page '),
-        h('input', {value: props.currPage, placeholder: props.currPage}),
-        h('span', {}, ' sur ' + props.maxNbPages),
-        h('button', { onclick: () => { if (props.currPage > 1)                props.onBackward() } }, '<'),
-        h('button', { onclick: () => { if (props.currPage < props.maxNbPages) props.onForward() } }, '>')
-    ])
+export default (props) => {
+    const leftEnabled  = props.currPage > 1
+    const rightEnabled = props.currPage < props.maxNbPages
+    return (
+        h('div', {class: 'pageNavigation'}, [
+            h('span', {}, 'Page '),
+            h('input', {value: props.currPage, placeholder: props.currPage}),
+            h('span', {}, ' sur ' + props.maxNbPages),
+            h('button', { disabled: !leftEnabled,  onclick: () => { if (leftEnabled)  props.onBackward() } }, '<'),
+            h('button', { disabled: !rightEnabled, onclick: () => { if (rightEnabled) props.onForward() } }, '>')
+        ]))
+    }
