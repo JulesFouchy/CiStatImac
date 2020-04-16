11a66b0e57d507c1cafd662ec872ef9b6463a7f2
Jules Fouchy <jules.fouchy@ntymail.com>
2020-04-16 20:01:31 +0200

    Vertically center the elements of PageNavigation !

--- src/components/PageNavigation.js
+++ src/components/PageNavigation.js
@@ -6,10 +6,14 @@ export default (props) => {
     const rightEnabled = props.currPage < props.maxNbPages - 1
     return (
         h('div', {class: 'pageNavigation'}, [
-            h('span', {}, 'Page '),
-            h('input', {value: props.navContent, placeholder: props.currPage + 1, oninput: props.onInputText}),
-            h('span', {}, ' sur ' + props.maxNbPages),
-            h('button', { disabled: !leftEnabled,  onclick: () => props.onBackward() }, '<'),
-            h('button', { disabled: !rightEnabled, onclick: () => props.onForward()  }, '>')
+            h('div', {class: 'pageNavInput'}, [
+                h('span', {}, 'Page '),
+                h('input', {value: props.navContent, placeholder: props.currPage + 1, oninput: props.onInputText}),
+                h('span', {}, ' sur ' + props.maxNbPages)
+            ]),
+            h('div', {class: 'pageNavButtons'}, [
+                h('button', { disabled: !leftEnabled,  onclick: () => props.onBackward() }, '<'),
+                h('button', { disabled: !rightEnabled, onclick: () => props.onForward()  }, '>')
+            ])
         ]))
     }
