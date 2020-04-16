8813843fc58bab4ad1e1baa4a082a98142d1bb6b
Jules Fouchy <jules.fouchy@ntymail.com>
2020-04-16 10:28:50 +0200

    Remove notWorking code on cmp::Box

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,11 +2,4 @@ import { h } from 'hyperapp'
 
 // basic componant with props
 export default (props) =>
-    h(
-        'div',
-        {
-            class: 'box',
-            style: 'width=100px'// + props.width + 'px height=' + ((props.height+'px')||'auto')
-        },
-        props.content
-    )
+    h('div', { class: 'box' }, props.content)
