28cc50bddd14a82c3cbf9619fa65f3a88b2c03ab
Jules Fouchy <jules.fouchy@ntymail.com>
2020-04-16 19:50:26 +0200

    Always display "28 Likes" on two separate lines

--- src/components/Citation.js
+++ src/components/Citation.js
@@ -6,7 +6,10 @@ import Ranking from './RankingNumber'
 export default (props) =>
     h('div', {class: 'citationContainer'}, [
         Ranking({number: props.ranking}),
-        Box({ content: props.nbLikes + ' Likes' }),
+        Box({ content: [
+            h('div', {}, props.nbLikes),
+            h('div', {}, 'Likes')
+        ]}),
         h('span', {class: 'citationTextAndAuthor'}, [
             h('p', {class: 'citationText'}, '"' + props.text + '"'),
             h('span', {class: 'citationAuthor'}, 'par ' + props.author)
