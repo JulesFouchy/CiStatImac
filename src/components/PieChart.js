c577aada2197b2f910bfc35b5a6da83b27b26dd5
Jules Fouchy <jules.fouchy@ntymail.com>
2020-04-20 20:47:25 +0200

    PieChart : add percentages

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -28,7 +28,22 @@ export default (props) =>
                                 usePointStyle: true,
                                 boxWidth: 6
                             }
-                        }
+                        },
+                        tooltips: { // Thanks to https://stackoverflow.com/questions/37257034/chart-js-2-0-doughnut-tooltip-percentages/49717859#49717859
+                            callbacks: {
+                                label: (tooltipItem, data) =>  {
+                                    const dataset = data.datasets[tooltipItem.datasetIndex]
+                                    const meta = dataset._meta[Object.keys(dataset._meta)[0]]
+                                    const total = meta.total
+                                    const currentValue = dataset.data[tooltipItem.index]
+                                    const percentage = parseFloat((currentValue/total*100).toFixed(1))
+                                    return ' ' + currentValue + ' (' + percentage + '%)'
+                                },
+                                title: (tooltipItem, data) => {
+                                    return data.labels[tooltipItem[0].index]
+                                }
+                            }
+                        },
                     }
                 })
                 props.acquireData(chart)
