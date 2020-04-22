03339ac121e6f157fa42c5c16481f99017fa2f40
Jules Fouchy <jules.fouchy@ntymail.com>
2020-04-22 22:04:48 +0200

    Fix fauteDeFrançais + Release build

--- src/views/Intro.js
+++ src/views/Intro.js
@@ -9,7 +9,7 @@ export default () => {
             id: 'intro',
             title: 'CiStat\'Imac',
             children: [
-                h('p', {class: 'introText'}, 'Cela fait 1 an que vous avez pris en main le Citat\'Imac. Vous l\'avez aimé, vous l\'avez nourris... Voyons le résultat !'),
+                h('p', {class: 'introText'}, 'Cela fait 1 an que vous avez pris en main le Citat\'Imac. Vous l\'avez aimé, vous l\'avez nourri... Voyons le résultat !'),
                 h('a', {href: 'http://citatimac.alwaysdata.net/', class:'citatimacButton'},
                     Box({content:
                         'Citat\'Imac'
